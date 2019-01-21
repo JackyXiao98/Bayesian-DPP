a7ce644d951838aa29e4ecb30d7ece1affb89264
JackyXiao98 <1025965245@qq.com>
2019-01-21 16:15:35 +0800

    delete dataset

--- bayesian_user.py
+++ bayesian_user.py
@@ -3,8 +3,6 @@ import os
 import numpy as np
 from utils import logging
 from collections import defaultdict
-import pdb
-from ddp import fast_greedy_map, fast_window_map_dpp
 import time
 
 
@@ -13,13 +11,13 @@ def parameter_setting():
     parser.add_argument('--is_log', default=True)
     parser.add_argument('--random_state', default=0)
     parser.add_argument('--verbose', default=2)
-    parser.add_argument('--lam_da', default=10.0)
+    parser.add_argument('--lam_da', default=0.1)
     parser.add_argument('--sigma', default=1.)
     parser.add_argument('--num_recommendation', default=5)
     parser.add_argument('--hidden_dim', default=10)
     parser.add_argument('--user_dim', default=4831)
     parser.add_argument('--movie_dim', default=3497)
-    parser.add_argument('--num_bandit_iter', default=20)
+    parser.add_argument('--num_bandit_iter', default=10)
     parser.add_argument('--dpp_theta', default=0.5)
     parser.add_argument('--dpp_w_size', default=5)
     parser.add_argument('--N', default=3497.)
@@ -35,11 +33,6 @@ def f_user_ratings_new():
             u, i, r = data[0], data[1], data[2]
             user_history[int(u)][int(i)] = float(r)
     return user_history
-
-
-def f_alpha(d, t, m, lam_da, sigma, s):
-    # return np.sqrt(d*np.log((1+t*m/lam_da)/sigma)) + np.sqrt(lam_da)*s
-    return 1.
 
 
 def rho(x):
@@ -62,8 +55,8 @@ def bayes_greedy_map(scores, movie_embs, K, theta):
     """
     C = defaultdict(list)
     alpha = theta / (1 - theta)
-    vec = rho(scores)
-    vec = np.exp(scores)
+    vec = np.sqrt(rho(scores))
+    vec = vec*np.exp(9*scores)
 
     D = vec * vec
     D = D * np.sum(movie_embs * movie_embs, axis=1)
@@ -74,11 +67,11 @@ def bayes_greedy_map(scores, movie_embs, K, theta):
     for k in range(1, K):
         Z = Z - set(Y)
         for i in Z:
-            # pdb.set_trace()
+            # define similarity matrix
             Sji = (np.sum(movie_embs[j] * movie_embs[i]) + 1) / 2
-            # Lji = scores[j] * scores[i] * Sji
+            # define L kernel
             Lji = vec[j] * vec[i] * Sji
-            # pdb.set_trace()
+            
             if len(C[i]) == 0 or len(C[j]) == 0:
                 ei = Lji / (D[j] ** 0.5)
             else:
@@ -86,7 +79,8 @@ def bayes_greedy_map(scores, movie_embs, K, theta):
             C[i].append(ei)
             D[i] = D[i] - ei ** 2
         ii = np.array(list(Z))
-        jj = np.argmax(D[ii]+rho(scores)[ii])
+        # greedy search for the next item
+        jj = np.argmax(D[ii])
         j = ii[jj]
         Y.append(j)
 
@@ -114,7 +108,6 @@ def bayesian_dpp(embeddings, new_index, test_items, args,
     for t in range(num):
         # theta_hat = np.random.multivariate_normal(vector_m, matrix_s)
         theta_hat = vector_m
-        # assert theta_hat.shape == vector_m.shape
         p_hat = rho(np.dot(theta_hat, embeddings))
         
         # get recommendation set s
@@ -125,47 +118,50 @@ def bayesian_dpp(embeddings, new_index, test_items, args,
         m = vector_m.reshape(hidden_dim, 1)
         xi_tmp = np.dot(x.T, matrix_s+np.dot(m, m.T))            # shape=(m_s, d)
         xi = np.sqrt(np.sum(xi_tmp.T*x, axis=0))                 # shape=(m_s)
-        eta_tmp = np.dot(embeddings.T, matrix_s + np.dot(m, m.T))    # shape=(m, d)
-        eta = np.sqrt(np.sum(eta_tmp.T * embeddings, axis=0))      # shape=(m)
+        # eta_tmp = np.dot(embeddings.T, matrix_s + np.dot(m, m.T))    # shape=(m, d)
+        # eta = np.sqrt(np.sum(eta_tmp.T * embeddings, axis=0))      # shape=(m)
         
         inv_matrix_s = np.linalg.inv(matrix_s)
         lam_xi = np.tile(lam_da(xi), (hidden_dim, 1))
-        # assert lam_xi.shape == (hidden_dim, args.num_recommendation)
         s_i = np.sum((lam_xi*x).T[:, None, :]*x.T[:, :, None], axis=0)
         
-        const = np.sum(embeddings * embeddings, axis=0)
-        # assert const.shape == (num_movies, )
-        miu_eta = np.tile(miu(const, eta), (hidden_dim, 1))
-        # assert miu_eta.shape == (hidden_dim, num_movies)
-        s_j = np.sum((miu_eta * embeddings).T[:, None, :] * embeddings.T[:, :, None], axis=0)
+        # const = np.sum(embeddings * embeddings, axis=0)
+        # miu_eta = np.tile(miu(const, eta), (hidden_dim, 1))
+        # s_j = np.sum((miu_eta * embeddings).T[:, None, :] * embeddings.T[:, :, None], axis=0)
         
+        # here we get updated covariance matrix S
         inv_matrix_s_post = inv_matrix_s + 2*s_i  # + 2*s_j/args.N
         matrix_s = np.linalg.inv(inv_matrix_s_post)
-        # assert matrix_s.shape == (hidden_dim, hidden_dim)
+        
+        # here we get our reward Y
         reward = np.array([1.0 if i in test_items else 0.0 for i in s_inx])
         
         m_i = np.sum(np.tile((reward+3./2), (hidden_dim, 1))*x, axis=1)
-        # assert m_i.shape == (hidden_dim, )
-        m_j = np.sum(embeddings, axis=1)
-        # assert m_j.shape == (hidden_dim, )
+        # m_j = np.sum(embeddings, axis=1)
+        
+        # here we get updated mean vector m
         vector_m_post = np.dot(inv_matrix_s, vector_m)+m_i
         vector_m = np.dot(matrix_s, vector_m_post)
-        # assert vector_m.shape == (hidden_dim, )
 
         # compute precision
         s_new = s_inx.copy()
+        # reindex our set because we delete some items
         for i in range(args.dpp_w_size):
             s_new[i] = new_index[s_inx[i]]
             s_all.append(s_new[i])
+        
+        # precision for each iteration
         inter_set = set(s_new).intersection(set(list(test_items.keys())))
         prec_curr = float(len(inter_set)) / float(args.num_recommendation)
         prec.append(prec_curr)
         
+        # mean precision for all iteration
         inter_set_all = set(s_all).intersection(set(list(test_items.keys())))
         prec_curr_all = float(len(inter_set_all)) / float(
             args.num_recommendation*args.num_bandit_iter)
         prec_all.append(prec_curr_all)
         
+        # delete the recommended itmes
         embeddings = np.delete(embeddings, s_inx, 1)
         nor_embs = np.delete(nor_embs, s_inx, 0)
         new_index = np.delete(new_index, s_inx)
@@ -192,7 +188,7 @@ if __name__ == '__main__':
         length = 0.
         for user in test_user_ratings.keys():
             index = np.arange(0, args.movie_dim)
-            if len(test_user_ratings[user]) > 100:
+            if len(test_user_ratings[user]) > 20:
                 mv_embs = movie_embs.copy()
                 prec = bayesian_dpp(mv_embs, index,
                                     test_user_ratings[user], args,
